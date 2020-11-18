6b3e51b47807a92bb202e49198f8e7fe664f1537
Marie <robloxfilmcam@gmail.com>
2020-11-18 23:22:59 +0100

    comply with the deprecation

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,3 +1,3 @@
-export { encode, decode } from "https://deno.land/std@v0.64.0/encoding/utf8.ts";
+export { encode, decode } from "https://deno.land/std@0.64.0/encoding/utf8.ts";
 
 export { prepare } from "https://deno.land/x/plugin_prepare@v0.7.2/mod.ts";
